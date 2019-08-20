93fd4ec22788c106ea13d0bd75b4e9c1eb3cd12a
League Student <league@iMac-34.attlocal.net>
2019-08-19 18:27:33 -0700

    week27 :smile:
    good done primeornot and checkpoint

---
+++ src/_01_prime_or_not/Primeornot.java
@@ -0,0 +1,26 @@
+package _01_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class Primeornot {
+public static void main(String[] args) {
+ String number =	JOptionPane.showInputDialog("Give This comupter a number");
+int abc = Integer.parseInt(number);
+ boolean isPrime= true ;
+
+for (int i = 2; i < abc; i++) {
+	
+	int remainder = abc % i;
+	if (remainder == 0) {
+		JOptionPane.showMessageDialog(null, number+  " is not prime.");
+	isPrime=false;
+	break;
+	}
+	
+}
+ if(isPrime  ) {
+		JOptionPane.showMessageDialog(null, number+" is Prime");
+	return;
+	} 
+}
+}
---
+++ src/_07_skill_practice/CheckpointLevel0Module3.java
@@ -0,0 +1,14 @@
+package _07_skill_practice;
+
+public class CheckpointLevel0Module3 {
+public static void main(String[] args) {
+	for (int i = 0; i < 500; i++) {
+	int odd = i%2;
+	if (odd!=0) {
+		
+	
+		System.out.println(i);
+	}
+}
+}
+}
