f4bfe78c40ce56442005d6ad772c68ce0f5c5582
League Student <league@iMac-34.attlocal.net>
2019-09-02 18:31:18 -0700

    week28 :smile:
    I unleashed garhy and his minions on circle ring done circle ring

--- src/_05_circle_ring/CircleRing.java
+++ src/_05_circle_ring/CircleRing.java
@@ -7,29 +7,63 @@ package _05_circle_ring;
 import org.jointheleague.graphical.robot.Robot;
 
 public class CircleRing {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        // 1. Make a new Robot
+		Robot[] robots = new Robot[30];
+		// 1. Make a new Robot
+		for (int i = 0; i < robots.length; i++) {
+			robots[i] = new Robot();
+			robots[i].setX(150 + i * 10);
+			robots[i].setY(200);
+			robots[i].setSpeed(i % 11);
+			robots[i].miniaturize();
+			robots[i].setRandomPenColor();
+			robots[i].penDown();
+			for (int j = 0; j < 360; j++) {
+				robots[i].move(3);
+				robots[i].turn(1);
+				if (j % 20 == 0) {
+					for (int k = 0; k < 360; k++) {
+						robots[i].move(1);
+						robots[i].turn(1);
+					}
+				}
+			}
+		}
+		Robot garhy = new Robot();
 
+		// 2. Set your robot’s position to x=150 and y=200
+		garhy.setX(150);
+		garhy.setY(200);
 
-       //  2. Set your robot’s position to x=150 and y=200
+		// 3. Put the robot's pen down
+		garhy.miniaturize();
 
-        // 3. Put the robot's pen down
+		garhy.setRandomPenColor();
+		garhy.penDown();
+		// 4. Set the robot’s speed to 10
+		garhy.setSpeed(100);
+		// 5. Do everything below here 360 times (use i as the counter)
+		for (int i = 0; i < 360; i++) {
+			garhy.move(3);
+			garhy.turn(1);
+			if (i % 20 == 0) {
+				for (int j = 0; j < 360; j++) {
+					garhy.move(1);
+					garhy.turn(1);
+				}
+			}
+		}
+		// 6. Move the robot 3 pixels
 
-        // 4. Set the robot’s speed to 10
+		// 7. Turn the robot 1 degree
 
-        // 5. Do everything below here 360 times (use i as the counter)
-
-	        // 6. Move the robot 3 pixels
-
-       //  7. Turn the robot 1 degree
-
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
+		// 8. If the counter i is divisible by 20 (hint: use mod operator %)..
 
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
 
-				// 10. Move the robot 1 pixel
+		// 10. Move the robot 1 pixel
 
-				// 11. Turn the robot 1 degree
-}
+		// 11. Turn the robot 1 degree
+	}
 }
