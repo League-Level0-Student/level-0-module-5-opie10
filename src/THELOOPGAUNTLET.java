ea126886c47a223c03accc791b8d0a186f66a30a
League Student <league@iMac-34.attlocal.net>
2019-09-23 18:32:34 -0700

    week 31 :smile:
    working on the for loop gauntlet.

--- src/THELOOPGAUNTLET.java
+++ src/THELOOPGAUNTLET.java
@@ -1,4 +1,55 @@
 
 public class THELOOPGAUNTLET {
+public static void main(String[] args) {
+	for (int i = 0; i <101; i++) {
+	System.out.println(i);	
+	}
+	
+	for (int j = 100; j > -1; j--) {
+		System.out.println(j);
+	}
+	for (int k = 0; k < 100; k++) {
+  if (k%2==0) {
+		System.out.println(k);
+	}
+	}
+	for (int l = 0; l < 100; l++) {
+		  if (l%2>0) {
+				System.out.println(l);
+			}
+			}
+	for (int m = 0; m < 501 ; m++) {
+		  if (m%2>0) {
+				System.out.println(m+ " odd");
+			}
+		  else {
+			System.out.println(m + " even" );
+		}
+			}
+for (int n = 0; n < 12; n++) {
+	System.out.println("In "+(2008+ n) + " I was " +n);	
+}
+for (int i = 0; i <3; i++) {
+	for (int j = 0; j < 3; j++) {
+	System.out.println(i+ " "+j);
+	
+	}
+}
+	for (int i = 0; i <3; i++) {
+		for (int j = 0; j < 3; j++) {
+		System.out.println(i+ " "+j);
+		
+		}
+}
 
+for (int i = 1; i <4; i++) {
+	
+	for (int j = 1; j < 4; j++) {
+		for (int k = 1; k < 4; k++) {
+			System.out.println(i+ " "+j + " " +k);
+			
+			}
+	}
+}
+}
 }
